ec11016dc98e40dbfbdd0bf09b9f32ecbbe50cf7
hoanghai1310 <hoanghai.131096@gmail.com>
2022-09-12 10:58:17 +0700

    upDB12092022

--- src/main/java/com/example/casebe6/models/AppUser.java
+++ src/main/java/com/example/casebe6/models/AppUser.java
@@ -11,7 +11,7 @@ import java.util.Set;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
     private String pass;
     @ManyToOne
